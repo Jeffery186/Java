f1b07e3dfdb45dd70ddacbcede40a98ca49010a6
zyhibook <zyh15637540388@gmail.com>
2021-05-19 18:24:25 +0800

    hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,18 @@
+public class hello_world {
+    public static void main(String [] args)
+    {
+        System.out.println("你好，世界！");
+        int vacationDay;
+        vacationDay=12;
+        System.out.println(vacationDay);
+        System.out.println(vacationDay);
+//        double salary;
+        double salary=65000;
+        long earthPopulation;
+        boolean done;//布尔值
+        System.out.println(salary);
+        var aaa=10;
+        System.out.println(aaa);
+    }
+}
+//注释
